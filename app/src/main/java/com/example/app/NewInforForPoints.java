11b647925d91aa6076fccb7a6bb588211ff15014
valerijszemlanikins <doggvalera@gmail.com>
2014-03-19 19:38:45 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/com/example/app/Informations.java

--- app/src/main/java/com/example/app/NewInforForPoints.java
+++ app/src/main/java/com/example/app/NewInforForPoints.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -73,6 +74,8 @@ public class NewInforForPoints extends Activity{
             textView1.setText(getString(R.string.thelivingorg4));
 //            ImageView imageView = (ImageView) findViewById(R.id.imageView);
 //            imageView.setImageResource(R.drawable.family);
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.familyfirst);
 
         }else if (vasa==5){
             TextView textView1 = (TextView) findViewById(R.id.textView);
@@ -81,6 +84,8 @@ public class NewInforForPoints extends Activity{
 //            imageView.setImageResource(R.drawable.family);
 //            Image image = (Image) findViewById(R.drawable.family);
 //            imageView.setImageDrawable(R.drawable.family);
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.familyfirst);
 
         }else if (vasa==6){
             TextView textView1 = (TextView) findViewById(R.id.textView);
@@ -88,41 +93,177 @@ public class NewInforForPoints extends Activity{
 //            ImageView imageView = (ImageView) findViewById(R.id.imageView);
 //            imageView.setImageResource(R.drawable.family);
 
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.familyfirst);
         }else if (vasa==7){
             TextView textView1 = (TextView) findViewById(R.id.textView);
             textView1.setText(getString(R.string.thelivingorg7));
 //            ImageView imageView = (ImageView) findViewById(R.id.imageView);
 //            imageView.setImageResource(R.drawable.family);
-
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.familyfirst);
         }
         else if (vasa==10){
             TextView textView1 = (TextView) findViewById(R.id.textView);
             textView1.setText(getString(R.string.thelivingorg7));
-
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
         }
         else if (vasa==11){
             TextView textView1 = (TextView) findViewById(R.id.textView);
             textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
 
         }
         else if (vasa==12){
             TextView textView1 = (TextView) findViewById(R.id.textView);
             textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
 
         }
         else if (vasa==13){
             TextView textView1 = (TextView) findViewById(R.id.textView);
             textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
 
         }
         else if (vasa==14){
             TextView textView1 = (TextView) findViewById(R.id.textView);
             textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
 
         }
         else if (vasa==15){
             TextView textView1 = (TextView) findViewById(R.id.textView);
             textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
+
+        } else if (vasa==16){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
+
+        }
+        else if (vasa==17){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
+
+        }
+        else if (vasa==18){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
+
+        }
+        else if (vasa==19){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.corpo);
+
+        }
+        else if (vasa==20){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        }
+        else if (vasa==21){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        }
+        else if (vasa==22){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        }
+        else if (vasa==23){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        }
+        else if (vasa==24){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        }
+        else if (vasa==25){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        } else if (vasa==26){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        }
+        else if (vasa==27){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+        } else if (vasa==28){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        } else if (vasa==29){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.girl);
+
+        }
+        else if (vasa==30){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.sales_people);
+
+        }
+        else if (vasa==31){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.sales_people);
+
+        }
+        else if (vasa==32){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.sales_people);
+
+        }
+        else if (vasa==33){
+            TextView textView1 = (TextView) findViewById(R.id.textView);
+            textView1.setText(getString(R.string.thelivingorg7));
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.sales_people);
 
         }
         {
