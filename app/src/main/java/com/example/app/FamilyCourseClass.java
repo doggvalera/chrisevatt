f58ebf0eda5e5cdba2c28ed4367b646988e1521d
violetine <violetabinka@gmail.com>
2014-03-20 11:24:27 +0100

    lialia

--- app/src/main/java/com/example/app/FamilyCourseClass.java
+++ app/src/main/java/com/example/app/FamilyCourseClass.java
@@ -12,12 +12,11 @@ import android.widget.ExpandableListView;
 import java.util.ArrayList;
 
 /**
- * Created by valerijszemlanikins on 16.03.14.
+ * Created by VIO on 3/20/14.
  */
 public class FamilyCourseClass extends Activity {
 
-
-//    String name1 = "1.Developing a living organisation model";
+    //    String name1 = "1.Developing a living organisation model";
 //    String name2 = "2.Developing collaborative relationships with all stakeholders";
 //    String name3 = "3.Collaborative relationship skills:";
 //    String name4 = "4.Creating a collaborative and empowering culture:";
@@ -26,17 +25,12 @@ public class FamilyCourseClass extends Activity {
 //    String name7 = "7.Measures:";
 //    String name8 = "8.Developing a living organisation model";
 //    String name9 = "9.Developing a living organisation model";
-
-
     String moreInf3  = "http://www.chrisevatt.com/services/family-businesses-succession";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.family_business_course);
-
-
         ExpandableListView listView = (ExpandableListView)findViewById(R.id.expandableFamBusiness);
-
         ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>();
 //         ArrayList<String> children1 = new ArrayList<String>();
         ArrayList<String> children2 = new ArrayList<String>();
@@ -57,21 +51,16 @@ public class FamilyCourseClass extends Activity {
 //        children1.add("dfsdfsdf");
 //
 //        children1.add("dfsdfsdf");
-
-
 //        children2.add(getString(R.string.livingorganisation7));
 //        children2.add(name);
         listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-
                 int findLayout =  0+childPosition;
                 Intent i = new Intent(getBaseContext(), NewInforForPoints.class);
 //                i.putExtra("NameOfPage",numberpage);
                 i.putExtra("NameOfPage",findLayout);
-
                 startActivity(i);
-
                 return false;
             }
         });
@@ -81,24 +70,19 @@ public class FamilyCourseClass extends Activity {
         //Создаем адаптер и передаем context и список с данными
         ExpListAdapter adapter = new ExpListAdapter(getApplicationContext(), groups);
         listView.setAdapter(adapter);
-
         Button btnMoreInf3 = (Button) findViewById(R.id.startupFamily);
-
-
         btnMoreInf3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //
-
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(moreInf3));
                 startActivity(browserIntent);
 //
             }
-
-
         });
-
     }
-
+    public void startModule2 (View view4) {
+        startActivity(new Intent(this, FamilyModuleClass.class));
+    }
 }
 
--- app/src/main/java/com/example/app/LivingOrg.java
+++ app/src/main/java/com/example/app/LivingOrg.java
@@ -1,24 +1,29 @@
 package com.example.app;
 
 import android.app.Activity;
+import android.app.ExpandableListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
+import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
-
 import java.util.ArrayList;
 
 /**
  * Created by VIO on 3/18/14.
  */
-public class LivingOrg extends Activity {
+public class LivingOrg extends Activity implements
+        ExpandableListView.OnChildClickListener {
 
     private static final String path ="http://www.boisestatefootball.com/sites/default/files/videos/original/01%20-%20coach%20pete%20bio_4.mp4";
     private VideoView video;
@@ -29,10 +34,7 @@ public class LivingOrg extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
-
-
+        setContentView(R.layout.living_org_info);
 //        mVideoURL = getIntent().getStringExtra("EXT_URL");
 //        Log.i("VIDEO URL", " " + mVideoURL);
 
@@ -45,26 +47,10 @@ public class LivingOrg extends Activity {
 
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.living_org_info);
-
-        ExpandableListView listView = (ExpandableListView)findViewById(R.id.LivingExpandable);
-
-        ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>();
-
-        ArrayList<String> children2 = new ArrayList<String>();
-
-        children2.add(getString(R.string.familyCousesesPoint1));
-        children2.add(getString(R.string.familyCousesesPoint2));
-        children2.add(getString(R.string.familyCousesesPoint3));
-        children2.add(getString(R.string.familyCousesesPoint4));
-        children2.add(getString(R.string.familyCousesesPoint5));
-        children2.add(getString(R.string.familyCousesesPoint6));
-        children2.add(getString(R.string.familyCousesesPoint7));
-
-        groups.add(children2);
-
-        ExpListAdapter adapter = new ExpListAdapter(getApplicationContext(), groups);
-        listView.setAdapter(adapter);
+        ExpandableListView expandbleLis = (ExpandableListView)findViewById(R.id.LivingExpandable);
+        expandbleLis.setDividerHeight(2);
+        expandbleLis.setGroupIndicator(null);
+        expandbleLis.setClickable(true);
 
         video = (VideoView) findViewById(R.id.videoLivingInfo);
         video.setVideoPath(path);
@@ -72,12 +58,62 @@ public class LivingOrg extends Activity {
         ctlr = new MediaController(this);
         ctlr.setMediaPlayer(video);
         video.setMediaController(ctlr);
-        video.start();
+        //video.start();
+        setGroupData();
+        setChildGroupData();
 
-
-
+        NewAdapter mNewAdapter = new NewAdapter(groupItem, childItem);
+        mNewAdapter
+                .setInflater(
+                        (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE),
+                        this);
+        expandbleLis.setAdapter(mNewAdapter);
+        expandbleLis.setOnChildClickListener(this);
+    }
+    public void setGroupData() {
+        groupItem.add("The Living Organisation");
+        groupItem.add("Purpose");
+        groupItem.add("The operating principles");
+        groupItem.add("Benefits");
     }
 
+    ArrayList<String> groupItem = new ArrayList<String>();
+    ArrayList<Object> childItem = new ArrayList<Object>();
 
+    public void setChildGroupData() {
+
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("It is any grouping of people working together for a common good of all stakeholders.\n" +
+                "These groups maybe communities, cities and social and business enterprises.\n" +
+                "Stakeholders generally include: employees; customers, members and users; suppliers;\n" +
+                "financial investors; the natural environment; the societies the groups work in.\n");
+        childItem.add(child);
+
+        child = new ArrayList<String>();
+        child.add("To give the principles and practices for creating and running an organisation\n" +
+                "that supports and is supported by all its stakeholders for their sustainable longevity.\n");
+        childItem.add(child);
+
+        child = new ArrayList<String>();
+        child.add("All stakeholders work or support what is their individual and collective best interests.\n" +
+                "Best interests is what supports and enhances their life as exemplified by how the human body functions.\n" +
+                "Operating motto: Leave everyone and everything better than when you found them.\n");
+        childItem.add(child);
+
+        child = new ArrayList<String>();
+        child.add("Because living organisations support and are supported by their stakeholders they are:\n" +
+                "stronger; more adaptable; can more quickly take advantage of change; are more creative;\n" +
+                "more innovative; evolve in relationship with their stakeholders and can co-create greater value with their stakeholders than they could alone.\n");
+        childItem.add(child);
     }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v,
+                                int groupPosition, int childPosition, long id) {
+        Toast.makeText(LivingOrg.this, "Clicked On Child",
+                Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
+
 
